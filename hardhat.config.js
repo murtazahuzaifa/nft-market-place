b40992a22194cc24253a9025b4706ae88847cbc5
murtazahuzaifa <murtaza.huzaifa2@gmail.com>
2021-10-26 23:45:40 +0500

    contract deploy script added and home page updated

--- hardhat.config.js
+++ hardhat.config.js
@@ -3,18 +3,24 @@ require("@nomiclabs/hardhat-waffle");
 const projectId = "c66526088eb147109136c1dabbdf61cf";
 
 module.exports = {
+  defaultNetwork: "hardhat",
   networks: {
     hardhat: {
       chainId: 1337
     },
-    mumbai: {
-      url: `https://polygon-mumbai.infura.io/v3/${projectId}`,
-      accounts: []
-    },
-    mainnet: {
-      url: `https://mainnet.infura.io/v3/${projectId}`,
-      accounts: []
-    },
+    // mumbai: {
+    //   url: `https://polygon-mumbai.infura.io/v3/${projectId}`,
+    //   accounts: []
+    // },
+    // mainnet: {
+    //   url: `https://mainnet.infura.io/v3/${projectId}`,
+    //   accounts: []
+    // },
   },
-  solidity: "0.8.4",
+  solidity: {
+    version: "0.8.4",
+    setting: {
+      optimizer: { enabled: true, runs: 200 }
+    }
+  },
 };
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,22 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import '../styles/globals.css';
+import Link from 'next/link';
+import type { AppProps } from 'next/app';
+
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <div>
+      <nav className='border p' >
+        <p className='text-4xl font-bold' >Metaverse Marketplace</p>
+        <div className='flex mt-4'>
+          <Link href="/"><a className='mr-6 text-pink-500' >Home</a></Link>
+          <Link href="/create-item"><a className='mr-6 text-pink-500' >Sell Digital Asset</a></Link>
+          <Link href="/my-assets"><a className='mr-6 text-pink-500' >My Digital Asset</a></Link>
+          <Link href="/create-dashboard"><a className='mr-6 text-pink-500' >Creator Dashboard</a></Link>
+        </div>
+      </nav>
+      <Component {...pageProps} />
+    </div>
+  )
 }
 export default MyApp
